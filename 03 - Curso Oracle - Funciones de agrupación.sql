fd60c12676e14f6f434a2732bb75ac6dca74d39d
Hernán Darío Velásquez O <Hernn Daro Velsquez O>
2017-05-09 22:25:25 -0500

    se completa la unidad, referente a GROUP BY, HAVING y funciones de agrupación

---
+++ 03 - Curso Oracle - Funciones de agrupación.sql
@@ -0,0 +1,76 @@
+SELECT country_name
+FROM countries
+WHERE country_name LIKE 'A%';
+
+
+
+---------------------------------------------------
+--video 26
+--AVG(DISTINCT | ALL columna)
+
+SELECT AVG(5) FROM employees;
+--el avg de un entero es el entero
+
+SELECT round(AVG(salary),2) FROM employees;
+
+--promedio de los salarios distintos
+SELECT round(AVG(DISTINCT salary),2) FROM employees;
+
+SELECT AVG(commission_pct) FROM employees;
+
+--cantidad tiempo que han trabajado en el area de programación
+
+SELECT last_name, job_id, ROUND((SYSDATE - hire_date) / 365.25, 2) "Años Trbajados"
+FROM employees
+WHERE job_id = 'IT_PROG';
+
+SELECT ROUND(AVG((SYSDATE - hire_date) / 365.25), 2) "Años Trbajados"
+FROM employees
+WHERE job_id = 'IT_PROG';
+
+-----------------------------------
+--funcion de agregado
+--video27
+--SUM([DISTINCT | ALL])
+
+
+SELECT SUM(salary) from employees;
+--todos cuyos valores no son nulos
+
+SELECT salary FROM employees WHERE salary IN NULL;
+
+SELECT sum(2) from employees;
+--suma 2 por cada registro en el resustado de la consulta
+
+SELECT SUM(DISTINCT salary) FROM employees;
+
+--solo agrega los no null
+SELECT SUM(commission_pct) FROM employees;
+SELECT SUM(DISTINCT commission_pct) FROM employees;
+
+--cuantos años han trabajado todos los mienbros de la compañia hasta la fecha actual
+SELECT ROUND(SUM(SYSDATE - hire_date) / 365.25, 2) "Total Años"
+FROM employees;
+
+--solo recibe numeros, no fechas o cadenas
+------------------------------------------------
+--video 28
+--MIN([DISTINCT | ALL], expresion)
+--MAX([DISTINCT | ALL], expresion)
+--operan con fecha, numero, caracter, entre otros
+
+SELECT MIN(commission_pct), MAX(commission_pct) FROM employees;
+
+SELECT MIN(start_date), MAX(end_date) FROM job_history;
+
+--con usa cadena
+SELECT MIN(job_id), MAX(job_id) FROM employees;
+
+SELECT MIN(hire_date), MIN(salary), MAX(hire_date), MAX(salary)
+FROM employees
+WHERE JOB_ID = 'SA_REP';
+
+--calular el promedio de la longitud de el nombre de todos los paises, redondear al siguiente entero
+
+SELECT ROUND(AVG(LENGTH(country_name))) "Promedio de Longitudes"
+FROM countries;   
---
+++ 03 - Curso Oracle - GROUP BY.sql
@@ -0,0 +1,125 @@
+--video 29
+--Cláusula GROUP BY
+
+ SELECT AVG(salary) FROM employees;
+ --si quiero ver el promedio pero por cada departamento
+ 
+ SELECT department_id,  ROUND(AVG(salary))
+ FROM employees
+ GROUP BY department_id
+ ORDER BY 2 DESC;
+ 
+ -- para poder agrupar por una caracteristica, esta debe estar en el select
+ 
+ --buscar el munero de empleados que tiene cada uno de lo smanager
+ SELECT COUNT(*), manager_id
+ FROM employees
+ GROUP BY manager_ID
+ ORDER BY 1 DESC, 2 NULLS LAST;
+ --NULLS LAST=> los null al final
+ 
+ 
+ --buscar maximo salaro y el conteo de empleados por departamento
+   SELECT MAX(salary), COUNT(*)
+   FROM employees
+   GROUP BY department_id
+   ORDER BY department_id;
+   
+   --buscar numero paises por region en la tabla paises
+   SELECT region_id, COUNT(*)
+   FROM countries
+   GROUP BY region_id
+   ORDER BY region_id;
+   
+   --buscar los empleados que na renunciado por cada año
+   SELECT TO_CHAR(end_date, 'yyyy') "Año",
+      COUNT(*) Empleados
+    FROM job_history
+    GROUP BY TO_CHAR(end_date, 'yyyy')
+    ORDER BY COUNT(*) DESC; 
+    --ordenando por una expresion
+    
+    SELECT TO_CHAR(end_date, 'yyyy') "Año",
+      COUNT(*) Empleados
+    FROM job_history
+    GROUP BY TO_CHAR(end_date, 'yyyy')
+    ORDER BY TO_CHAR(end_date, 'yyyy') DESC; 
+    
+    
+    SELECT end_date, COUNT(*)
+    FROM job_history;  --arroja error
+    --todas las columnas que no son de agrupación deben estar en un grup by
+    SELECT end_date, COUNT(*)
+    FROM job_history
+    GROUP BY end_date;
+    
+    SELECT end_date, start_date, COUNT(*)
+    FROM job_history
+    GROUP BY end_date; --arroja error
+    
+    SELECT end_date, start_date, COUNT(*)
+    FROM job_history
+    GROUP BY end_date, start_date; 
+    
+    
+    --video 30
+    --GROUP BY
+    
+    SELECT department_id, SUM(commission_pct)
+    FROM employees
+    WHERE commission_pct IS NOT NULL
+    GROUP BY department_id;
+    
+    SELECT department_id, job_id, SUM(commission_pct)
+    FROM employees
+    WHERE commission_pct IS NOT NULL
+    GROUP BY department_id, job_id;
+    
+    SELECT department_id, job_id, SUM(commission_pct)
+    FROM employees
+    WHERE commission_pct IS NOT NULL
+    GROUP BY department_id, job_id;
+    --podemos agregar hasta 255 columnas en la agrupación
+    
+    --Ejercicio
+    
+    SELECT TO_CHAR(end_date, 'yyyy'), job_id, COUNT(*)
+    from job_history
+    GROUP BY TO_CHAR(end_date, 'yyyy'), job_id
+    ORDER BY COUNT(*) DESC;
+    
+    /*
+    seleccionar la longitud del nombre mas corto y el mas largo
+    de los empleados que sean representantes de ventas
+    */
+    
+    SELECT MAX(LENGTH(last_name)), MIN(LENGTH(last_name))
+    FROM employees
+    WHERE job_id = 'SA_REP';
+    
+    ---------------------------------------------------------
+    --video 31
+    --HAVING => filtrar y refinar los resultados de las consultas basados en grupos
+    
+    SELECT department_id
+    FROM job_history
+    WHERE  department_id IN (50, 60, 80, 110);
+    
+    --conteno de empleados en el departamentoq ue cambiaron de trabajo
+    
+    SELECT department_id, COUNT(*)
+    FROM job_history
+    WHERE  department_id IN (50, 60, 80, 110)
+    GROUP BY department_id;
+    --cuando usanod WHERE, la cluasula GROUP BY va despues de esta
+    --si nos dijeran mayor a 1
+    --no podemos hacer funciones de agrupacion fuera de GROUP BY
+    --no podemos usar el AND en el WHERE
+    --aqui debemos usar HAVING despues de GROUP BY
+    SELECT department_id, COUNT(*)
+    FROM job_history
+    WHERE  department_id IN (50, 60, 80, 110)
+    GROUP BY department_id
+    HAVING COUNT(*) > 1;
+     
+    
